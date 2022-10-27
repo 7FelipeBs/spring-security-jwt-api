a6673b3b1ab72686cb31dbf96baa878caf52fd73
Felipe B Silva <programming1129@gmail.com>
2022-10-26 22:21:50 -0300

    Mudanças em algumas entidades e dtos para usar o lombok

--- src/main/java/com/felipebs/seguranca/entidades/CargoAcesso.java
+++ src/main/java/com/felipebs/seguranca/entidades/CargoAcesso.java
@@ -2,7 +2,14 @@ package com.felipebs.seguranca.entidades;
 
 import javax.persistence.*;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "roles")
 public class CargoAcesso {
   @Id
@@ -12,28 +19,4 @@ public class CargoAcesso {
   @Enumerated(EnumType.STRING)
   @Column(length = 20)
   private CargoAcessoEnum name;
-
-  public CargoAcesso() {
-
-  }
-
-  public CargoAcesso(CargoAcessoEnum name) {
-    this.name = name;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public CargoAcessoEnum getName() {
-    return name;
-  }
-
-  public void setName(CargoAcessoEnum name) {
-    this.name = name;
-  }
 }
